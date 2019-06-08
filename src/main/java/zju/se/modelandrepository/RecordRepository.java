791d0075e94c6bc5b0826d38b20bb73a79d8a9c2
jzy1999 <1148464672@qq.com>
2019-06-08 19:05:58 +0800

    add Record and Repository

---
+++ src/main/java/zju/se/modelandrepository/RecordRepository.java
@@ -0,0 +1,11 @@
+package zju.se.modelandrepository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+import zju.se.modelandrepository.Record;
+
+public interface RecordRepository extends JpaRepository<Record,Integer>{
+
+}
